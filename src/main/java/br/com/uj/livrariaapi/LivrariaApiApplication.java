f5eea436aac77138122c01381d97d15eb8408511
jenivaldo pereira de souza junior <jenivaldojuniorjr@gmail.com>
2025-05-28 01:14:10 -0300

    feat: commit

--- src/main/java/br/com/uj/livrariaapi/LivrariaApiApplication.java
+++ src/main/java/br/com/uj/livrariaapi/LivrariaApiApplication.java
@@ -3,8 +3,9 @@ package br.com.uj.livrariaapi;
 import br.com.uj.livrariaapi.model.entities.UsuarioModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class LivrariaApiApplication {
 
     public static void main(String[] args) {

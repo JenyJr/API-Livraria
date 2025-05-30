c7e97c83fe54f5299a00efcc51d37ce3ff12d266
jenivaldo pereira de souza junior <jenivaldojuniorjr@gmail.com>
2025-05-29 22:52:19 -0300

    feat: create dto response

--- src/main/java/br/com/uj/livrariaapi/model/dtos/LoginResponseDTO.java
+++ src/main/java/br/com/uj/livrariaapi/model/dtos/LoginResponseDTO.java
@@ -3,5 +3,6 @@ package br.com.uj.livrariaapi.model.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginResponseDTO(@NotBlank String status,
-                               Integer id) {
+                               Integer idUsuario,
+                               String nome) {
 }
---
+++ src/main/java/br/com/uj/livrariaapi/model/dtos/UsuarioDTOError.java
@@ -0,0 +1,5 @@
+package br.com.uj.livrariaapi.model.dtos;
+
+public record UsuarioDTOError(String status,
+                              String msg) {
+}
